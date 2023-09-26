4a0cefb858034b64e164c46746e2e1d12cbf947d
Tijmen De Bruine <tijmendebruine@Tijmens-MacBook-Pro.local>
2023-09-26 15:28:23 +0200

    parse_error_check exit code fixed

--- incl/exec.h
+++ incl/exec.h
@@ -6,7 +6,7 @@
 /*   By: dvan-kle <dvan-kle@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/08/23 13:42:56 by dvan-kle      #+#    #+#                 */
-/*   Updated: 2023/09/26 15:14:53 by daniel        ########   odam.nl         */
+/*   Updated: 2023/09/26 15:21:01 by tijmendebru   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,5 @@ int	cd(const char *path);
 void	pwd(void);
 void    ignore_signals(void);
 void	ft_free(char **ptr);
-bool	execute_builtin(t_cmd_table *cmd_table, t_env_list *env_list);
 
 #endif
